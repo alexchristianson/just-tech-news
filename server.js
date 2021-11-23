b79ffba9c80c06ad9d837dba0a61424b07e2e800
Alex C <alex.christianson.05@gmail.com>
2021-11-23 12:41:08 -0600

    added optimization

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -21,12 +20,19 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const routes = require('./controllers');
 
 // turn on routes
 app.use(routes);
